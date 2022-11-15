c478d3861e8c1b00e3e713d662561e94670a458f
Skuukzkyy <109440860+Skuukzkyy@users.noreply.github.com>
2022-11-15 12:02:55 +0800

    DISPLAYED ALL MOVIE FROM DB TO MOVIE PAGE

--- movie.php
+++ movie.php
@@ -1,5 +1,7 @@
 <?php
 require "connection.php";
+$current_date = date('Y-m-d');
+$next_month_date = date('Y-m-d', strtotime('+1 month'));
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +27,7 @@ require "connection.php";
           </nav>
       </header>
 
-  <!-- Movies tab---------------------------------------------------------->
+  <!-- TOP PICKS---------------------------------------------------------->
   <div class="search-container">
     <input id="searchBar" type="search" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
   </div>
@@ -34,7 +36,7 @@ require "connection.php";
   <div class="container">
     <?php
       $rows = mysqli_query($conn, "SELECT * FROM tickets_tbl INNER JOIN movie_tbl ON tickets_tbl.movie_id = movie_tbl.movie_id ORDER BY tickets_tbl.sold_ticket DESC LIMIT 10");
-      foreach ($rows as $row): ?>
+      foreach ($rows as $row): $id = $row['movie_id']?>
         <div class="box"></a>
           <div class="imgBox">
             <img src="img/<?php echo $row['banner2'] ?>">
@@ -42,75 +44,71 @@ require "connection.php";
           <div class="details">
             <div class="content">
               <h2><?php echo $row['movie_title'] ?></h2>
-              <p><?php echo $row['movie_description'] ?></p><br>
+              <p>
+                <?php $movie_category = mysqli_query($conn, "SELECT movie_category.movie_title, category_tbl.category FROM movie_category INNER JOIN category_tbl ON movie_category.category_id = category_tbl.category_id INNER JOIN movie_tbl ON movie_category.movie_title = movie_tbl.movie_title WHERE movie_tbl.movie_id = '$id'"); ?>
+                <?php foreach ($movie_category as $category) {
+                  echo $category['category'].", ";
+                } ?>  
+              </p><br>
               <a href="movie_tab.php?id=<?php echo $row['movie_id'] ?>"><button class="btn_buy"> Buy Tickets</button></a>
             </div>
           </div>
         </div>
       <?php endforeach?>
-
   </div>
 
   <!-- NEW RELEASES---------------------------------------------------------->
   <hr><br>
   <p class="head">NEW RELEASES</p>
   <div class="container">
-        <div class="box">
+  <?php
+      $rows = mysqli_query($conn, "SELECT * FROM movie_tbl WHERE movie_start BETWEEN '$current_date' AND '$next_month_date'");
+      foreach ($rows as $row): $id = $row['movie_id']?>
+        <div class="box"></a>
           <div class="imgBox">
-            <img src="media/m1.jpg">
+            <img src="img/<?php echo $row['banner2'] ?>">
           </div>
           <div class="details">
-            <div class="content">
-              <h2>One Pice Film Red </h2>
-              <p> One Piece Film: Red is a 2022 Japanese animated fantasy action-adventure film directed by Gorō Taniguchi and produced by Toei Animation.</p><br>
-              <button class="btn_buy"> Buy Tickets</button>
+            <div id="content" class="content">
+              <h2><?php echo $row['movie_title'] ?></h2>
+              <p>
+                <?php $movie_category = mysqli_query($conn, "SELECT movie_category.movie_title, category_tbl.category FROM movie_category INNER JOIN category_tbl ON movie_category.category_id = category_tbl.category_id INNER JOIN movie_tbl ON movie_category.movie_title = movie_tbl.movie_title WHERE movie_tbl.movie_id = '$id'"); ?>
+                <?php foreach ($movie_category as $category) {
+                  echo $category['category'].", ";
+                } ?> 
+              </p><br>
+              <a href="movie_tab.php?id=<?php echo $row['movie_id'] ?>"><button class="btn_buy"> Buy Tickets</button></a>
             </div>
           </div>
         </div>
-
-        <div class="box">
-          <div class="imgBox">
-            <img src="media/m5.jpeg">
-          </div>
-          <div class="details">
-            <div class="content">
-              <h2>Armin Arlelt </h2>
-              <p> a soldier in the Scout Regiment. He is also a childhood friend of Eren Jaeger and Mikasa Ackermann, and is one of the two deuteragonists of the series.</p>
-              <button class="btn_buy"> Buy Tickets</button>
-            </div>
-          </div>
-        </div>
-        </div>
+      <?php endforeach?>
+  </div>
 
   <!-- ALL MOVIES---------------------------------------------------------->
   <hr><br>
   <p class="head">ALL MOVIES</p>
   <div class="container">
-        <div class="box">
+    <?php
+      $rows = mysqli_query($conn, "SELECT * FROM movie_tbl ORDER BY movie_title LIMIT 10");
+      foreach ($rows as $row): $id = $row['movie_id']?>
+        <div class="box"></a>
           <div class="imgBox">
-            <img src="media/m6.jfif">
+            <img src="img/<?php echo $row['banner2'] ?>">
           </div>
           <div class="details">
             <div class="content">
-              <h2>One Pice Film Red </h2>
-              <p> One Piece Film: Red is a 2022 Japanese animated fantasy action-adventure film directed by Gorō Taniguchi and produced by Toei Animation.</p><br>
-              <button class="btn_buy"> Buy Tickets</button>
+              <h2><?php echo $row['movie_title'] ?></h2>
+              <p>
+                <?php $movie_category = mysqli_query($conn, "SELECT movie_category.movie_title, category_tbl.category FROM movie_category INNER JOIN category_tbl ON movie_category.category_id = category_tbl.category_id INNER JOIN movie_tbl ON movie_category.movie_title = movie_tbl.movie_title WHERE movie_tbl.movie_id = '$id'"); ?>
+                <?php foreach ($movie_category as $category) {
+                  echo $category['category'].", ";
+                } ?>  
+              </p><br>
+              <a href="movie_tab.php?id=<?php echo $row['movie_id'] ?>"><button class="btn_buy"> Buy Tickets</button></a>
             </div>
           </div>
         </div>
-
-        <div class="box">
-          <div class="imgBox">
-            <img src="media/m10.jpg">
-          </div>
-          <div class="details">
-            <div class="content">
-              <h2>Armin Arlelt </h2>
-              <p> a soldier in the Scout Regiment. He is also a childhood friend of Eren Jaeger and Mikasa Ackermann, and is one of the two deuteragonists of the series.</p>
-              <button class="btn_buy"> Buy Tickets</button>
-            </div>
-          </div>
-        </div>
-        </div><hr>
+      <?php endforeach?>
+  </div><hr>
 </body>
 </html>
